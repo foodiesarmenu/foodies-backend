83866adeb877dba837edfbe9058ea20380e14328
foodiesarmenu <160359176+foodiesarmenu@users.noreply.github.com>
2024-02-26 19:30:50 +0200

    mobile-restaurant-getAll (#8)
    Co-authored-by: Ehab-Tarek <ehabtarek144@gmail.com>

---
+++ src/mobile/restaurant/restaurant.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { RestaurantService } from './restaurant.service';
+import { FindAllQuery, FindAllResponse, Role, Roles, swagger } from 'src/common';
+import { Restaurant } from 'src/models';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@Roles(Role.Client)
+@ApiTags(swagger.MobileRestaurant)
+@Controller('mobile/restaurant')
+export class RestaurantController {
+  constructor(private readonly restaurantService: RestaurantService) { }
+
+  @ApiOperation({ summary: 'Get all restaurants' })
+  @Get()
+  async getAll(@Query() query: FindAllQuery) {
+    const getAllRestaurantsResponse = new FindAllResponse<Restaurant>();
+
+    try {
+      const restaurants = await this.restaurantService.getAll(query);
+      getAllRestaurantsResponse.success = true;
+      getAllRestaurantsResponse.data = restaurants.data;
+      getAllRestaurantsResponse.currentPage = restaurants.currentPage;
+      getAllRestaurantsResponse.numberOfPages = restaurants.numberOfPages;
+      getAllRestaurantsResponse.numberOfRecords = restaurants.numberOfRecords;
+    } catch (error) {
+      getAllRestaurantsResponse.success = false;
+      throw error;
+    }
+
+    return getAllRestaurantsResponse;
+  }
+}
