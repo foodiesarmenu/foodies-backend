0b45f746933a346d934ef2a93d6f240bfa7c3276
foodiesarmenu <160359176+foodiesarmenu@users.noreply.github.com>
2024-03-13 00:49:37 +0200

    Change password (#17)
    * changePassword
    
    * Refactor password change functionality.
    
    ---------
    
    Co-authored-by: Mahmoud Sakr <93199475+mahmoud08sakr@users.noreply.github.com>
    Co-authored-by: Ehab-Tarek <ehabtarek144@gmail.com>

--- src/mobile/auth/auth.controller.ts
+++ src/mobile/auth/auth.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Patch, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { swagger } from '../../common/constants/swagger.constant';
 import { Public } from '../../common/decorators/public.decorator';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login-customer.dto';
+import { ChangePasswordDto } from './dto/ChangePasswordDto';
+import { changePasswordResponse } from 'src/common';
+import { Client } from 'src/models';
 
 @ApiTags(swagger.MobileAuth)
 @Controller('mobile/auth')
@@ -23,4 +26,27 @@ export class AuthController {
   async login(@Request() req: Express.Request) {
     return this.authService.login(req.user);
   }
+
+
+
+  @ApiOperation({ summary: 'Change password' })
+  @Patch('change-password')
+  async changePassword(
+    @Body() changePasswordDto: ChangePasswordDto,
+    @Request() req: Express.Request
+  ) {
+    const PasswordResponse = new changePasswordResponse<Client>();
+    try {
+      const password = await this.authService.changePassword(
+        changePasswordDto,
+        req.user['_id']);
+      PasswordResponse.success = true;
+      PasswordResponse.data = password;
+    } catch (error) {
+      PasswordResponse.success = false;
+      throw error;
+    }
+    return PasswordResponse;
+  }
 }
+
--- src/mobile/auth/auth.service.ts
+++ src/mobile/auth/auth.service.ts
@@ -1,9 +1,12 @@
-import { ConflictException, Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { omit } from 'lodash';
 import { ClientRepository } from 'src/models';
 import { Client } from '../client/entities/client.entity';
+
+import { ChangePasswordDto } from './dto/ChangePasswordDto';
+import { message } from 'src/common/constants/message.constant';
 
 @Injectable()
 export class AuthService {
@@ -53,4 +56,38 @@ export class AuthService {
   public signJwt(payload: any) {
     return this.jwtService.sign(payload);
   }
+
+  async changePassword(changePasswordDto: ChangePasswordDto, userId: string) {
+    try {
+
+
+      if (changePasswordDto.newPassword !== changePasswordDto.confirmPassword) {
+        throw new BadRequestException(message.user.PasswordNotMatch)
+      }
+
+      const user = await this.clientRepository.getOne({ _id: userId });
+
+      if (!user) {
+        throw new ConflictException(message.user.NotFound);
+      }
+
+      if (!bcrypt.compareSync(changePasswordDto.currentPassword, user.password)) {
+        throw new ConflictException(message.user.InvalidPassword);
+      }
+
+      const hashedPassword = bcrypt.hashSync(changePasswordDto.newPassword, 10);
+      const updatedUser = await this.clientRepository.update(
+        { _id: userId },
+        { password: hashedPassword },
+        { new: true }
+      );
+      console.log(updatedUser, 'updatedUser');
+
+      return updatedUser;
+    } catch (error: any) {
+      this.handleError(error);
+    }
+  }
+
+
 }
