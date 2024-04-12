6d47804327150ba72353430c1794780dcf41f153
Ehab-Tarek <ehabtarek144@gmail.com>
2024-04-12 23:18:51 +0200

    Fix import statement in PromotionRepository

--- src/models/promotion/promotion.repository.ts
+++ src/models/promotion/promotion.repository.ts
@@ -1,7 +1,7 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AbstractRepository } from '../abstract.repository';
-import { Promotion, PromotionDocument } from './Promotion.schema';
+import { Promotion, PromotionDocument } from './Promotion.schema'; 
 
 export class PromotionRepository extends AbstractRepository<Promotion> {
     constructor(
